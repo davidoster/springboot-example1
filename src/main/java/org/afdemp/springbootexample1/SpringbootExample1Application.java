7ce7c68a9989cee062ec702458a87a68ccc3db34
David Oster <paspa@hotmail.com>
2020-05-12 13:02:45 +0300

    Initial

---
+++ src/main/java/org/afdemp/springbootexample1/SpringbootExample1Application.java
@@ -0,0 +1,14 @@
+package org.afdemp.springbootexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExample1Application.class, args);
+                System.out.println("Test");
+	}
+
+}
